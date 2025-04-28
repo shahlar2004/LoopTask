daac8b0304e858a6cd59537ed1584626805ef48c
jakoismo <shahlarismayilov304@gmail.com>
2025-04-28 11:56:49 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,215 @@
+﻿namespace LoopTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task1
+
+
+            //int n = 50;
+            //int eded= Convert.ToUInt16(Console.ReadLine());
+
+
+            //if (eded > n)
+            //{
+            // Console.WriteLine("Duzgun eded daxil et");
+            //}
+
+            //else
+            //{
+            //    for (int j = 1; j <= eded; j++)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+            //}
+
+
+            //Task2
+
+            //int N=int.Parse(Console.ReadLine());
+            //int tekler = 0;
+
+            //if(N >=1 && N <= 100)
+            //{
+            //    for(int i = 1; i <= N; i++)
+            //    {
+            //        if (i%2==1)
+            //        {
+            //            tekler += i;
+            //            Console.WriteLine("i="+i);
+
+            //}
+            //    }
+            //    Console.WriteLine("Cem="+tekler);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Duzgun bir eded daxil et!");
+            //}
+
+
+            //Task4
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            ////  int s=int.Parse(Console.ReadLine());
+            //int eded1 = 0;
+            //int eded2 = 0;
+            //if (n > 1 && n <= 6)
+            //{
+            //    string ededs1 = "1", ededs2 = "1";
+
+            
+            //    for (int i = 1; i < n; i++)
+            //    {
+   
+            //        ededs1 += "0";
+            //        ededs2 += "0";
+            //    }
+
+            //    ededs2 += "0";
+
+            //    eded1 = int.Parse(ededs1.Trim());
+            //    eded2 = int.Parse(ededs2.Trim());
+            //}
+
+
+            //Console.Write("s=");
+            //int s = int.Parse(Console.ReadLine());
+            //int k = 0;
+
+            
+            
+            //if (s >= 0 && s <= 54)
+            //{
+            //    for (int i = eded1; i < eded2; i++)
+            //    {
+            //        string f=i.ToString();
+            //        for (int j=0;j<f.Length;j++)
+            //        {
+            //            k += Convert.ToInt32(f[j].ToString());
+            //        }
+
+            //        if (k==s)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //        k = 0;
+            //    }
+            //}
+
+
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+
+
+            //for (int i = a; i <= b; i++) 
+            //{
+            //    Console.WriteLine("---------------------");
+            //    Console.WriteLine("kv="+ Math.Pow(i,2));
+            //    Console.WriteLine("kub="+ Math.Pow(i,3));
+            //}
+
+
+            //Task5
+
+            //int n=int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <=n; i++)
+            //{
+            //    if (Math.Pow(i,2)<=n)
+            //    {
+            //        Console.WriteLine(Math.Pow(i, 2));
+            //    }
+            //}
+
+
+            //Task6
+
+            //string eded=Console.ReadLine();
+            //string reqem=Console.ReadLine();    
+            //int say = 0;
+
+            //for (int i = 0; i < eded.Length; i++) 
+            //{
+            //    if (eded[i]==Convert.ToChar(reqem))
+            //    {
+            //        say++;
+            //    }
+            //}
+
+            //Console.WriteLine(say);
+
+            //Task7
+
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //int cem = 0;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    cem += i; 
+            //}
+
+            //Console.WriteLine(cem);
+
+            //task8 
+
+            //int n=int.Parse(Console.ReadLine());
+            //int say = 0;
+            //for (int i = 1; i <= n; i++) 
+            //{
+            //    if (n%i==0)
+            //    {
+            //        say++;
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //Console.WriteLine("say"+say);
+
+
+            //Task9
+
+            //int n = int.Parse(Console.ReadLine());
+            //int say = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (n % i == 0 && i%2==1)
+            //    {
+            //        say++;
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //Console.WriteLine("say=" + say);
+
+
+            ////Task9
+            //int n=int.Parse(Console.ReadLine()),ed1=0,ed2=0,ed3=0;
+
+            //for (int i = 100; i < 1000; i++) 
+            //{
+            //    ed1 = i / 100;
+            //    ed2 = (i/ 10)%10;
+            //    ed3 = (i % 10);
+
+            //    if (ed1+ed2+ed3==n)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+
+        }
+    }
+}
